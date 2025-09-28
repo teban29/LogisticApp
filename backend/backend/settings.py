527a2a7431c81c58ce9b27f35ccc93701e49e1f0
teban29 <ecardonagonzalez1029@gmail.com>
2025-09-27 19:17:38 -0500

    cors de desarrollo

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -228,6 +228,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://localhost:3000",
     "http://127.0.0.1:5173",
     "http://31.97.10.251:5173",
     "http://31.97.10.251",
